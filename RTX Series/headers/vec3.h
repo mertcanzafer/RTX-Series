7ca1533b7eebcfc786f5c278452b75eee56e95b7
mrtzfr <mertcanzafer06@gmail.com>
2024-06-19 11:01:51 +0300

    vec3 Operators

--- RTX Series/headers/vec3.h
+++ RTX Series/headers/vec3.h
@@ -101,3 +101,71 @@ public:
 private:
 
 };
+
+// Alias to vec3
+using point3 = vec3;
+
+// Vector utility functions
+
+inline std::ostream& operator<<(std::ostream& os, const vec3& v)
+{
+	os << v.v.x << " " << v.v.y <<" " << v.v.z;
+	return os;
+}
+
+inline vec3 operator+(const vec3& v1, const vec3& v2)
+{
+	return vec3(v1.v.x + v2.v.x , v1.v.y + v2.v.y , v1.v.z + v2.v.z);
+}
+
+inline vec3 operator-(const vec3& v1, const vec3& v2)
+{
+	return vec3(v1.v.x - v2.v.x, v1.v.y - v2.v.y, v1.v.z - v2.v.z);
+}
+
+inline vec3 operator*(const vec3& v1, const vec3& v2)
+{
+	return vec3(v1.v.x * v2.v.x, v1.v.y * v2.v.y, v1.v.z * v2.v.z);
+}
+
+inline vec3 operator*(double k, const vec3& v)
+{
+	return vec3(v.v.x * k, v.v.y * k , v.v.z * k);
+}
+
+inline vec3 operator*(const vec3& v,double k)
+{
+	return k * v;
+}
+
+inline vec3 operator/(const vec3& v, double k)
+{
+	if (k != 0.0)
+	{
+		return 1 / k * v;
+	}
+	else {
+		throw "Divided by zero expetion!!\n";
+	}
+}
+
+inline double dot(const vec3& v1, const vec3& v2)
+{
+	vec3 mult = v1 * v2;
+	return(mult.v.x + mult.v.y + mult.v.z);
+}
+
+inline vec3 cross(const vec3& v1, const vec3& v2)
+{
+	return vec3
+	(
+		v1.v.y * v2.v.z - v1.v.z * v2.v.y,
+		(v1.v.x * v2.v.z - v1.v.z * v2.v.x) * -1,
+		v1.v.x * v2.v.y - v1.v.y * v2.v.x
+	);
+}
+
+inline vec3 unit_vector(const vec3& v)
+{
+	return v / v.length();
+}
